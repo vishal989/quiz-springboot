04f1a10d279e3b39a7ac0f0cd6f6b19aaf39fee2
vishal989 <vavishalarya@gmail.com>
2024-02-16 16:45:31 +0530

    controller

---
+++ src/main/java/com/learning/quizapp/controller/QuestionController.java
@@ -0,0 +1,15 @@
+package com.learning.quizapp.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+    @GetMapping("allQuestions")
+    public String getAllQuestions(){
+        return "hi, this is arya";
+    }
+}
