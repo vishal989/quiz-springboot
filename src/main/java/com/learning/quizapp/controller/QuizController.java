b6b4a14bfa574394881f12533993f7c7c80591b2
vishal989 <vavishalarya@gmail.com>
2024-02-19 17:37:06 +0530

    responses

--- src/main/java/com/learning/quizapp/controller/QuizController.java
+++ src/main/java/com/learning/quizapp/controller/QuizController.java
@@ -1,13 +1,19 @@
 package com.learning.quizapp.controller;
 
+import com.learning.quizapp.model.Question;
+import com.learning.quizapp.model.QuestionWrapper;
+import com.learning.quizapp.model.Response;
 import com.learning.quizapp.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping
+@RequestMapping("quiz")
 public class QuizController {
+
     @Autowired
     QuizService quizService;
 
@@ -15,4 +21,15 @@ public class QuizController {
     public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title){
         return quizService.createQuiz(category, numQ, title);
     }
+
+    @GetMapping("get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id){
+        return quizService.getQuizQuestions(id);
+    }
+
+    @PostMapping("submit/{id}")
+    public ResponseEntity<Integer> submitQuiz(@PathVariable Integer id, @RequestBody List<Response> responses){
+        return quizService.calculateResult(id, responses);
+    }
+
 }
