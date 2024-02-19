8b2ddbc99d356c2b8391a57d36d93e17421e8e15
vishal989 <vavishalarya@gmail.com>
2024-02-19 15:09:17 +0530

    response entity

--- src/main/java/com/learning/quizapp/controller/QuestionController.java
+++ src/main/java/com/learning/quizapp/controller/QuestionController.java
@@ -4,6 +4,7 @@ package com.learning.quizapp.controller;
 import com.learning.quizapp.Question;
 import com.learning.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,17 +15,17 @@ public class QuestionController {
     @Autowired
     QuestionService questionService;
     @GetMapping("allQuestions")
-    public List<Question> getAllQuestions(){
+    public ResponseEntity<List<Question>> getAllQuestions(){
         return questionService.getAllQuestions();
     }
 
     @GetMapping("category/{category}")
-    public List<Question> getQuestionsByCategory(@PathVariable String category){
+    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category){
         return questionService.getQuestionsByCategory(category);
     }
 
     @PostMapping("add")
-    public String addQuestion(@RequestBody Question question){
+    public ResponseEntity<String> addQuestion(@RequestBody Question question){
         return questionService.addQuestion(question);
     }
 }
